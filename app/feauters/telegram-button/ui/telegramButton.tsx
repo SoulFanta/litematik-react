85b7771714b5ca27ddb8689ed4715d6c6e5c95d9
maximvinogradov88@gmail.com <s1ndzidef@gmail.com>
2025-08-11 21:21:17 +1000

    Изменена кнопка интеграции с телеграм

--- app/feauters/telegram-button/ui/telegramButton.tsx
+++ app/feauters/telegram-button/ui/telegramButton.tsx
@@ -13,7 +13,8 @@ export default function TelegramLogin() {
     script.src = "https://telegram.org/js/telegram-widget.js?22";
     script.async = true;
     script.setAttribute("data-telegram-login", "MinecraftLitematicAuthBot");
-    script.setAttribute("data-size", "large");
+    script.setAttribute("data-size", "medium");
+    script.setAttribute("data-userpic", "false");
     script.setAttribute("data-onauth", "onTelegramAuth(user)");
     host.appendChild(script);
 
