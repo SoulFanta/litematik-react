9f6c4936cb417071f0e82d1400098b6a235828b0
maximvinogradov88@gmail.com <s1ndzidef@gmail.com>
2025-08-11 21:18:15 +1000

    Немного переработан header

--- app/widgets/header/ui/header.tsx
+++ app/widgets/header/ui/header.tsx
@@ -1,12 +1,14 @@
+import TelegramLogin from "~/feauters/telegram-button/ui/telegramButton";
 import "../styles/header.scss";
-import TelegramLogin from "./../../telegram-button/ui/telegramButton";
 
 export default function Header() {
   return (
     <header className="header ">
       <div className="header__wrapper container">
-          <h3 className="header__title">Litematik<span className="header__title-beta">beta</span></h3>
-          <TelegramLogin />
+        <h3 className="header__title">
+          Litematik<span className="header__title-beta">beta</span>
+        </h3>
+        <TelegramLogin />
       </div>
     </header>
   );
