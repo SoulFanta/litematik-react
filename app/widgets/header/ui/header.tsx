6f102a1eaccb80ea5451149ba97005a01725a1b9
maximvinogradov88@gmail.com <s1ndzidef@gmail.com>
2025-08-11 23:36:52 +1000

    Добавлена ссылка на главную страницу по клику на название. так же header сделал в sticky режиме

--- app/widgets/header/ui/header.tsx
+++ app/widgets/header/ui/header.tsx
@@ -1,13 +1,16 @@
 import TelegramLogin from "~/feauters/telegram-button/ui/telegramButton";
 import "../styles/header.scss";
+import { Link } from "react-router";
 
 export default function Header() {
   return (
     <header className="header ">
       <div className="header__wrapper container">
-        <h3 className="header__title">
-          Litematik<span className="header__title-beta">beta</span>
-        </h3>
+        <Link to="/">
+          <h3 className="header__title">
+            Litematik<span className="header__title-beta">beta</span>
+          </h3>
+        </Link>
         <TelegramLogin />
       </div>
     </header>
