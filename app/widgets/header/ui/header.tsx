fc514fc7e8da24d0006fd40e0e07f1c5694312ab
maximvinogradov88@gmail.com <s1ndzidef@gmail.com>
2025-08-11 19:18:41 +1000

    Переместил header из фич в виджеты

--- app/widgets/header/ui/header.tsx
+++ app/widgets/header/ui/header.tsx
@@ -4,7 +4,7 @@ import TelegramLogin from "./../../telegram-button/ui/telegramButton";
 export default function Header() {
   return (
     <header className="header ">
-      <div className="header__wrapper">
+      <div className="header__wrapper container">
           <h3 className="header__title">Litematik<span className="header__title-beta">beta</span></h3>
           <TelegramLogin />
       </div>
