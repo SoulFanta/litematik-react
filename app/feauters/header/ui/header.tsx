a2e185f799c42f909e72816b923fbd48dfeec0fc
maximvinogradov88@gmail.com <s1ndzidef@gmail.com>
2025-08-11 16:48:32 +1000

    Стили  для шапки сделаны и шапка готова

--- app/feauters/header/ui/header.tsx
+++ app/feauters/header/ui/header.tsx
@@ -1,9 +1,13 @@
-import "../styles/header.scss"
+import "../styles/header.scss";
+import TelegramLogin from "./../../telegram-button/ui/telegramButton";
 
 export default function Header() {
   return (
-    <header className="header container">
-      <h2 className="header__title">Litematik</h2>
+    <header className="header ">
+      <div className="header__wrapper">
+          <h3 className="header__title">Litematik<span className="header__title-beta">beta</span></h3>
+          <TelegramLogin />
+      </div>
     </header>
   );
 }
