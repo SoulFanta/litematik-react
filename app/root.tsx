146eb9f55f3d36d9acf665ef68e7e6e12077c99c
maximvinogradov88@gmail.com <s1ndzidef@gmail.com>
2025-08-11 12:13:01 +1000

    Переименовал app.scss -> styles.scss так же поменял импорт в root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -8,7 +8,7 @@ import {
 } from "react-router";
 
 import type { Route } from "./+types/root";
-import "./app.scss";
+import "./styles.scss";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
