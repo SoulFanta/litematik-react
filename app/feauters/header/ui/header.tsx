bbf6108defd74526cd7bd7ddaebb52431fa02685
maximvinogradov88@gmail.com <s1ndzidef@gmail.com>
2025-08-11 13:08:38 +1000

    Header добавил импорт стилей

---
+++ app/feauters/header/ui/header.tsx
@@ -0,0 +1,9 @@
+import "../styles/header.scss"
+
+export default function Header() {
+  return (
+    <header className="header container">
+      <h2 className="header__title">Litematik</h2>
+    </header>
+  );
+}
