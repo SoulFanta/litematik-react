b65c197612661823192bff91d72785b77af20963
maximvinogradov88@gmail.com <s1ndzidef@gmail.com>
2025-08-12 00:20:18 +1000

    Тест description

--- app/widgets/projects-list/ui/project-list.tsx
+++ app/widgets/projects-list/ui/project-list.tsx
@@ -9,13 +9,16 @@ export default function ProjectList() {
         <Input placeholder="Введите запрос" size="sm" />
       </header>
       <div className="project-list__grid">
-        <ProjectCard title={"карточка1"} progress={0} />
+        <ProjectCard
+          title={"Замок"}
+          progress={0}
+          description="lorem imkdslfdksflsdf fdsfsdfnsdf dskfmsdlfmsd sdkfkmsd;lfmsdf sdlkfmsdlfds"
+        />
         <ProjectCard title={"карточка2"} progress={10} />
         <ProjectCard title={"карточка3"} progress={15} />
         <ProjectCard title={"карточка4"} progress={20} />
         <ProjectCard title={"карточка5"} progress={100} />
         <ProjectCard title={"карточка6"} progress={100} />
-      
       </div>
     </section>
   );
